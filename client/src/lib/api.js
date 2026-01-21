b2ade79028a1c6d173e51b3bdcd136e260b34e7f
iamcollins404 <61983049+iamcollins404@users.noreply.github.com>
2026-01-21 06:18:39 +0000

    Update API base URL to production endpoint for deployment readiness

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://fosssil-procure-api.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
